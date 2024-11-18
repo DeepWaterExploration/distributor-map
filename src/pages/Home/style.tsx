63c8197c21c2440770f2d9cfe461be7b85fd2ad2
Kunal Singla <tekunalogy@gmail.com>
2024-11-18 10:41:07 -0800

    fix spacing

--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -2,12 +2,12 @@ export const styles = {
   pageContainer: {
     width: "100%",
     height: "100vh",
+    background: "#262626"
   },
 
   mapContainer: {
     minWidth: "350px",
     width: "100%",
-    maxWidth: "2000px",
 
     height: "calc(100% - ( 1px / 1000))", //Full screen minus a miniscule amount
 

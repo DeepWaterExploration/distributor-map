fab14d0f3e4f11b2e098b58b492c478ef8214cbe
jdszekeres <jdszekeres@gmail.com>
2025-07-23 16:17:25 -0700

    Make map slightly more zoomed in and add robotshop

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,7 +54,7 @@ const HomePage: React.FC = () => {
     <Box sx={styles.pageContainer}>
       {/* @ts-ignore */}
       <MapContainer
-        minZoom={2}
+        minZoom={2.5}
         maxBounds={[
           [-90, -180],
           [90, 180],

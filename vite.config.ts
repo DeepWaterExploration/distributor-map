175993cbb2d9e1b0070a8a79d0053c2f885d6821
Kunal Singla <tekunalogy@users.noreply.github.com>
2025-08-26 10:10:52 -0700

    add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [million.vite({ auto: true, mute: true }), react()],
-  base: './'
+  base: '/distributor-map/'
 });
